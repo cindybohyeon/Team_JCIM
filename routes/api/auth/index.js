a08f58223a15f9142d669fd4065ebdb7644ca57f
Bohyeon Kim <55770848+980926a@users.noreply.github.com>
2021-09-24 18:39:23 +0900

    Update index.js

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -9,7 +9,7 @@ router.use("/findId", require("./findId"));
 router.use("/refresh", require("./refresh"));
 // router.use("/findPw", require("./findPw"));
 router.use("/check_duplicate_email", require("./check_duplicate_email"));
-
+router.use("/checkIot", require("./checkIot"));
 
 
 
