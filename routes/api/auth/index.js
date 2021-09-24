f39d6fee03a398a47cdc265d76f4eec3ae848835
soyeonnn <50646042+soyeonnn@users.noreply.github.com>
2021-09-24 15:25:20 +0900

    Update index.js

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -8,6 +8,7 @@ router.use("/signin", require("./signin"));
 router.use("/findId", require("./findId"));
 router.use("/refresh", require("./refresh"));
 // router.use("/findPw", require("./findPw"));
+router.use("/check_duplicate_email", require("./check_duplicate_email"));
 
 
 
