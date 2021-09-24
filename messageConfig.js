f295dd42bcbe2551eb2283424eab12039d49b700
Bohyeon Kim <55770848+980926a@users.noreply.github.com>
2021-09-24 20:15:36 +0900

    Delete messageConfig.js

--- messageConfig.js
+++
@@ -1,7 +0,0 @@
-const messageConfig = {
-    uri: 'ncp:sms:kr:271810352422:industry_university_link',
-    secretKey: 'HFxgGXlnBcFvPEFeUo68Cr1O2LwhuUwgdvCZymr5',
-    accessKey: '4zqxl4VXqVYVh1sTSrpR',
-    from: '01086542787'
-}
-module.exports = messageConfig;
