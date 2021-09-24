1eca41cb969f62e9d8168ac26cf8071716273437
soyeonnn <50646042+soyeonnn@users.noreply.github.com>
2021-09-24 19:55:52 +0900

    Update index.js

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -10,7 +10,7 @@ router.use("/refresh", require("./refresh"));
 // router.use("/findPw", require("./findPw"));
 router.use("/check_duplicate_email", require("./check_duplicate_email"));
 router.use("/checkIot", require("./checkIot"));
-
+router.use("/signup_phone_auth", require("./signup_phone_auth"));
 
 
 module.exports = router;
